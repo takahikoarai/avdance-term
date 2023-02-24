e30cbbf995aade6cf90b37d02812cb019da04584
takahikoarai <araitakahiko@gmail.com>
2023-02-25 01:31:13 +0900

    create 'attendaces' and 'rests' table and modified models

---
+++ database/migrations/2023_02_24_135842_create_rests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attendance_id')->constrained('attendance');
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rests');
+    }
+}
