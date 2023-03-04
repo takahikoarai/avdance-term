12c92b3618f9771057084af4fe8f62cae8d8a63b
takahikoarai <araitakahiko@gmail.com>
2023-03-05 00:20:07 +0900

    modified 'restEnd'

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -19,28 +19,29 @@ class AttendanceController extends Controller
         }
     }
 
+    //出勤アクション
     public function workStart()
     {
         //勤務開始時間を記録。「勤務開始」と「休憩終了」の文字がグレーに
         $user = Auth::user();
-        $oldworkStart = Attendance::where('user_id', $user->id)->latest()->first();
+        $oldattendance = Attendance::where('user_id', $user->id)->latest()->first();
 
         $oldDay = '';
 
         //退勤前に2回押せない制御
-        if($oldworkStart){
-            $oldAttendanceStartTime = new Carbon($oldworkStart->start_time);
+        if($oldattendance){
+            $oldAttendanceStartTime = new Carbon($oldattendance->start_time);
             $oldDay = $oldAttendanceStartTime->startOfday();//Carbonインスタンスを生成することで、starOfdayメソッドが使える
             $today = Carbon::today();
 
-            if(($oldDay == $today) && (empty($oldworkStart->end_time))){
+            if(($oldDay == $today) && (empty($oldattendance->end_time))){
                 return redirect()->back()->with('message','出勤打刻済みです');
             }
         }
 
         //退勤後に出勤を押せない制御
-        if($oldworkStart){
-            $oldAttendanceEndTime = new Carbon($oldworkStart->end_time);
+        if($oldattendance){
+            $oldAttendanceEndTime = new Carbon($oldattendance->end_time);
             $oldDay = $oldAttendanceEndTime->startofDay();
         }
 
@@ -59,30 +60,31 @@ class AttendanceController extends Controller
         return redirect()->back();
     }
 
+    //退勤アクション
     public function workEnd()
     {
         
     }
 
-    //レコードがない場合はcreateを実行、すでにレコードがある場合はstart_timeを更新
+    //休憩開始アクション
     public function restStart()
     {
         $user = Auth::user();//ユーザー認証
         $attendance = Attendance::where('user_id', $user->id)->latest()->first();//attendaceテーブルの最新のレコード1件を取得
-        $oldrestStart = Rest::where('attendance_id', $attendance->id)->first();//attendanceテーブルのidにひもづくrestテーブルのレコードのうち最新の1件を取得
+        $oldrest = Rest::where('attendance_id', $attendance->id)->first();//attendanceテーブルのidにひもづくrestテーブルのレコードのうち最新の1件を取得
 
         //休憩開始を連続で押すのを防ぎたい
-        if($oldrestStart->end_time){
+        if($oldrest->end_time){
 
         }
 
-        if($attendance->start_time&&$oldrestStart->start_time && !$attendance->end_time){
+        if($attendance->start_time && $oldrest->start_time && !$attendance->end_time){
             //勤務中＆休憩開始データがすでに存在する＆勤務終了していない、ならばstart_timeを更新
-            $oldreststart->update([
+            $oldrest->update([
                 'start_time' => Carbon::now()
             ]);
             return redirect()->back();
-        }elseif($attendance->start_time && !$oldrestStart->start_time && !$attendance->end_time){
+        }elseif($attendance->start_time && !$oldrest->start_time && !$attendance->end_time){
             //勤務中＆休憩開始データが存在しない＆勤務終了していない、ならばレコードを新規作成
             Rest::create([
                 'attendance_id' => $attendance->id,
@@ -92,11 +94,27 @@ class AttendanceController extends Controller
         }else{
             return redirect()->back();
         }
+
+        //「勤務開始」と「休憩開始」の文字がグレーに
     }
     
+    //休憩終了アクション
     public function restEnd()
     {
-        
+        $user = Auth::user();
+        $attendance = Attendance::where('user_id', $user->id)->latest()->first();
+        $oldrest = Rest::where('attendance_id', $attendance->id)->first();
+
+        //休憩終了を連続で押せない制御は？
+
+        //end_timeが存在しない場合は、end_timeを格納
+        if($oldrest->start_time && !$oldrest->end_time){
+            $oldrest->update([
+                'end_time' => Carbon::now(),
+            ]);
+            return redirect()->back();
+        }
+        return redirect()->back();
     }
 
     public function attendances()
